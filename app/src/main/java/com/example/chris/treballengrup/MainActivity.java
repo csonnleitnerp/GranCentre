7fdbda341cddc20ac5e6d6d60b9bfdbd81135edc
chris <christophersp21@gmail.com>
2017-11-03 10:34:59 +0100

    modi link al main

--- app/src/main/java/com/example/chris/treballengrup/MainActivity.java
+++ app/src/main/java/com/example/chris/treballengrup/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.chris.treballengrup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void opmenu (View v) {
+        startActivity(new Intent(this,Restaurante.class));
+        Intent i = new Intent(this,Restaurante.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/chris/treballengrup/Restaurante.java
+++ app/src/main/java/com/example/chris/treballengrup/Restaurante.java
@@ -1,7 +1,9 @@
 package com.example.chris.treballengrup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Restaurante extends AppCompatActivity {
 
@@ -10,4 +12,5 @@ public class Restaurante extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurante);
     }
+
 }
