d3723a2f08ea50ad905a100eefe7dc45bee20135
chris <christophersp21@gmail.com>
2017-11-06 13:31:17 +0100

    weather

--- app/src/main/java/com/example/chris/treballengrup/Restaurante.java
+++ app/src/main/java/com/example/chris/treballengrup/Restaurante.java
@@ -4,13 +4,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Spinner;
 
 public class Restaurante extends AppCompatActivity {
 
+    public Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurante);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setOnClickListener((View.OnClickListener) spinner);
     }
 
 }
