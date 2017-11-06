1001a743f512091049b95a65f2a74af5df46e357
Marfil <97marfiljavi@gmail.com>
2017-11-06 13:21:43 +0100

    Acabado Movies + Empezando Business + boton atrás

--- app/src/main/java/com/example/chris/treballengrup/Movies.java
+++ app/src/main/java/com/example/chris/treballengrup/Movies.java
@@ -1,7 +1,9 @@
 package com.example.chris.treballengrup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Movies extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class Movies extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies);
+    }
+
+    public void opmain (View v) {
+        startActivity(new Intent(this,MainActivity.class));
+        Intent i = new Intent(this,MainActivity.class);
+        startActivity(i);
     }
 }
