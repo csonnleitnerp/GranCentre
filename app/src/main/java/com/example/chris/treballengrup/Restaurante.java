cff2332d8e5696a488ab1979a5bd51ec33189a06
chris <christophersp21@gmail.com>
2017-11-10 16:15:36 +0100

    Weather restaurantes modi

--- app/src/main/java/com/example/chris/treballengrup/Restaurante.java
+++ app/src/main/java/com/example/chris/treballengrup/Restaurante.java
@@ -1,21 +1,108 @@
 package com.example.chris.treballengrup;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class Restaurante extends AppCompatActivity {
 
-    public Spinner spinner;
+
+    public Button web1;
+    public Button web2;
+    public Button web3;
+    public Button web4;
+    public Button web5
+
+    public Button tel1;
+    public Button tel2;
+    public Button tel3;
+    public Button tel4;
+    public Button tel5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurante);
-        spinner = (Spinner) findViewById(R.id.spinner);
-        spinner.setOnClickListener((View.OnClickListener) spinner);
+
+        web1 = (Button) findViewById(R.id.Web1);
+        web2 = (Button) findViewById(R.id.Web2);
+        web3 = (Button) findViewById(R.id.Web3);
+        web4 = (Button) findViewById(R.id.Web4);
+        web5 = (Button) findViewById(R.id.Web5);
+
+        tel1 = (Button) findViewById(R.id.Tel1);
+        tel2 = (Button) findViewById(R.id.Tel2);
+        tel3 = (Button) findViewById(R.id.Tel3);
+        tel4 = (Button) findViewById(R.id.Tel4);
+        tel5 = (Button) findViewById(R.id.Tel5);
+
+        web1.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        MostraMissatge("Hotel 1");
+                        AnarWeb("http://www.eltrull.com/ca");
+                    }
+                }
+        );
+
+        web2.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        MostraMissatge("Hotel 2");
+                        AnarWeb("http://www.viena.es/ca/restaurants/viena-granollers/");
+                    }
+                }
+        );
+
+        web3.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        MostraMissatge("Hotel 3");
+                        AnarWeb("http://www.hotelfondaeuropa.com/ca/restaurante/");
+                    }
+                }
+        );
+
+        web4.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        MostraMissatge("Hotel 4");
+                        AnarWeb("http://lamezzaluna.es/");
+                    }
+                }
+        );
+
+        web5.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        MostraMissatge("Hotel 4");
+                        AnarWeb("https://www.hotel-bb.es/hotel/barcelona-granollers/");
+                    }
+                }
+        );
+
     }
+
+    public void MostraMissatge(String s) {
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+    }
+
+    public void AnarWeb(String s) {
+        //href="s";
+        Uri uri= Uri.parse(s);
+        Intent intent= new Intent(android.content.Intent.ACTION_VIEW,uri);
+        startActivity(intent);
+    }
+
     public void opmain (View v) {
         startActivity(new Intent(this,MainActivity.class));
         Intent i = new Intent(this,MainActivity.class);
